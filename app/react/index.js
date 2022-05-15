ca66640849c5998b9753c338487524b8444fbac9
cypressyi <cypressyi1990@gmail.com>
2022-05-15 18:04:33 +0800

    Tab content add control input and mock data

--- app/react/index.js
+++ app/react/index.js
@@ -1,7 +1,9 @@
-import { Fragment } from 'react'
+import { useState, Fragment } from 'react'
 import { Tab } from '@headlessui/react'
 
-export default function index() {
+const mockData = ['1', '2', '3']
+
+export default function Index() {
   return (
     <Tab.Group>
       <Tab.List className={'pb-4'}>
@@ -55,15 +57,28 @@ export default function index() {
 }
 
 function Panel1() {
+  const [inputValue, setInputValue] = useState(mockData[0])
+  const handleSave = () => {
+    mockData[0] = inputValue
+  }
+
   return (
     <Tab.Panel>
       <div>Content 1</div>
       <div>
-        <input className="px-2 py-1 my-4 border-solid border-2" />
+        <input
+          className="px-2 py-1 my-4 border-solid border-2"
+          value={inputValue}
+          onChange={e => setInputValue(e.target.value)}
+        />
       </div>
       <div>
-        <button className="px-2 py-1 bg-indigo-600 text-white border-solid rounded-md">
-          save
+        <button
+          className="px-2 py-1 bg-indigo-600 text-white border-solid rounded-md"
+          type="button"
+          onClick={handleSave}
+        >
+          Save
         </button>
       </div>
     </Tab.Panel>
@@ -71,15 +86,27 @@ function Panel1() {
 }
 
 function Panel2() {
+  const [inputValue, setInputValue] = useState(mockData[1])
+  const handleSave = () => {
+    mockData[0] = inputValue
+  }
   return (
     <Tab.Panel>
       <div>Content 2</div>
       <div>
-        <input className="px-2 py-1 my-4 border-solid border-2" />
+        <input
+          className="px-2 py-1 my-4 border-solid border-2"
+          value={inputValue}
+          onChange={e => setInputValue(e.target.value)}
+        />
       </div>
       <div>
-        <button className="px-2 py-1 bg-indigo-600 text-white border-solid rounded-md">
-          save
+        <button
+          className="px-2 py-1 bg-indigo-600 text-white border-solid rounded-md"
+          type="button"
+          onClick={handleSave}
+        >
+          Save
         </button>
       </div>
     </Tab.Panel>
@@ -87,15 +114,27 @@ function Panel2() {
 }
 
 function Panel3() {
+  const [inputValue, setInputValue] = useState(mockData[2])
+  const handleSave = () => {
+    mockData[0] = inputValue
+  }
   return (
     <Tab.Panel>
       <div>Content 3</div>
       <div>
-        <input className="px-2 py-1 my-4 border-solid border-2" />
+        <input
+          className="px-2 py-1 my-4 border-solid border-2"
+          value={inputValue}
+          onChange={e => setInputValue(e.target.value)}
+        />
       </div>
       <div>
-        <button className="px-2 py-1 bg-indigo-600 text-white border-solid rounded-md">
-          save
+        <button
+          className="px-2 py-1 bg-indigo-600 text-white border-solid rounded-md"
+          type="button"
+          onClick={handleSave}
+        >
+          Save
         </button>
       </div>
     </Tab.Panel>
